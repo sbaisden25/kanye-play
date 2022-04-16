0fd89ce4ced44e16a98e9a15d9876ae877265c22
sbaisden25 <spbaisden@gmail.com>
2022-04-16 19:04:07 -0400

    Video addition

--- src/pages/runaway/Runaway.js
+++ src/pages/runaway/Runaway.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react"
 
 import RunawaySoundboard from "../../components/Soundboard/RunawaySoundboard";
+import Video from "../../components/Video/Video";
 
 class Runaway extends Component {
     render() {
         return (
 
             <div class="main-container">
+                
+                <Video embedId="xihvf-I0d2Q"/>
                 <RunawaySoundboard />
+                
             </div>
-
         )
     }
 }
