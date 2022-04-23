3f23a847125c714cc8d340090edbcddef7f1147b
sbaisden25 <spbaisden@gmail.com>
2022-04-23 15:39:49 -0400

    deploy

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
+
 const path = require('path');
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 
 const app = express()
 const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
